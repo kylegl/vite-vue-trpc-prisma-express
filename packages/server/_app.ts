04146754b4b75fee54e700d26047aa1b7b452328
Kyle <lauber.kyle@gmail.com>
2022-11-30 02:41:22 -0800

    feat: vitesse

--- packages/server/_app.ts
+++ packages/server/_app.ts
@@ -1,24 +1,24 @@
-import { fastifyTRPCPlugin} from '@trpc/server/adapters/fastify'
+import { fastifyTRPCPlugin } from '@trpc/server/adapters/fastify'
 import Fastify from 'fastify'
-import {createContext} from './router/createContext'
-import { appRouter } from './router/appRouter';
+import { createContext } from './router/createContext'
+import { appRouter } from './router/appRouter'
 
 const server = Fastify({
   maxParamLength: 5000,
-  logger: true
+  logger: true,
 })
 
 server.register(fastifyTRPCPlugin, {
   prefix: '/trpc',
   trpcOptions: { router: appRouter, createContext },
-});
+})
 
 const start = async () => {
   try {
-    await server.listen({port: 3000})
+    await server.listen({ port: 3000 })
     server.log.info(`🚀 Server is listening on ${server.server.address()}`)
-
-  } catch(e) {
+  }
+  catch (e) {
     server.log.error(e)
     process.exit(1)
   }
