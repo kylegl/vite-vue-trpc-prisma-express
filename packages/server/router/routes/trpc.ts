f29d68eb30400069c27f200f584fd61ba9f7a63e
Kyle <lauber.kyle@gmail.com>
2022-11-29 23:10:44 -0800

    chore: setup prisma and postgres docker

---
+++ packages/server/router/routes/trpc.ts
@@ -0,0 +1,9 @@
+import { initTRPC } from '@trpc/server'
+import {z} from 'zod'
+import superjson from 'superjson'
+
+const t = initTRPC.create({transformer: superjson})
+
+export const router = t.router
+export const middleware = t.middleware
+export const publicProcedure = t.procedure
